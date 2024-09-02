f297f397a45b275244eb0587abaabc4042121637
Torwikari <torwikari@gmail.com>
2024-09-02 07:09:27 +0200

    Add random choice

---
+++ rps.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+
+// get random: rock, paper or scissors from the computer
+function getComputerChoice() {
+  const choices = ["rock", "paper", "scissors"];
+  //   Generate random number from 0-2, round-down via math.floor
+  const random_number = Math.floor(Math.random() * 3);
+  //   Get a computer choice based on random number.
+  const choice = choices[random_number];
+  return choice;
+}
